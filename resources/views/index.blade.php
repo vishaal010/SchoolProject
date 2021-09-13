c9ba392136b347665de668fe8b14528b3a49fa6b
vishaal010 <36966434+vishaal010@users.noreply.github.com>
2021-09-13 21:33:42 +0200

    Front end
    Added a liitle bit of the Front end

---
+++ resources/views/index.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fotografie</title>
+    <link rel="stylesheet" href="{{asset('css/app.css') }}">
+</head>
+<body class="text-gray-600">
+
+<div>
+    <div>
+        <nav>
+            <div>
+                <h1 class="font-bold uppercase p-4 border-b border-gray-100">
+                    <a href="/">Fotografie</a>
+                </h1>
+            </div>
+            <ul>
+                <li class="text-gray-700">
+                    <a href="#">
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span>About</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span>Contact</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
+    <main class="px-16 py-6">
+        <div>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+
+        <header>
+            <h4 class="text-gray-700 text-6x1 font-semibold">Vind mooie Fotografie</h4>
+            <h4 class="text-2x1 font-semibold">Door verschillende mensen</h4>
+        </header>
+
+        <div>
+            <h4 class="font-bold mt-12 pb-2 border-b border-gray-100"> Laatste Foto's</h4>
+            <div class="mt-8">
+                <!-- Cards -->
+                <div>
+                    <img src="#" alt="">
+                    <div>
+                        <span> Mooie landschap</span>
+                        <span> Foto gemaakt door</span>
+                    </div>
+                </div>
+            </div>
+            <h4 class="font-bold mt-12 pb-2 border-b border-gray-100">Populaire Foto's</h4>
+        <div class="mt-8">
+        <!-- Cards -->
+        </div>
+            <div>
+                <div>
+                    Laad meer
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
+    extend: {
+        colors: {
+            primary: '#FF6363',
+            secondary: {
+                100: '#E2E2D5',
+                200: '#888883',
+            }
+        }
+    }
   },
   variants: {},
   plugins: [
